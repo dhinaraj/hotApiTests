be1009e837a261de4bb4b5f58dd50bd0dc88530f
Dhinakaran Rajkumar <mail2dheena@gmail.com>
2017-07-14 13:48:40 +0800

    First Commit

--- src/test/java/org/plus65/PartTimer.java
+++ src/test/java/org/plus65/PartTimer.java
@@ -1,14 +1,22 @@
 package org.plus65;
 
+
 import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.http.ContentType;
 import com.jayway.restassured.path.json.JsonPath;
 import com.jayway.restassured.response.Response;
 import com.jayway.restassured.response.ResponseBodyExtractionOptions;
+import com.jayway.restassured.config.LogConfig;
+import com.jayway.restassured.config.RestAssuredConfig;
+
+import static com.jayway.restassured.config.EncoderConfig.encoderConfig;
+import static com.jayway.restassured.config.RestAssuredConfig.config;
+import com.jayway.restassured.config.SSLConfig;
 
 import base.TestLogger;
 import base.TestBaseSetup;
 
+import org.apache.commons.io.output.WriterOutputStream;
 import org.apache.http.HttpStatus;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
@@ -19,34 +27,77 @@ import org.testng.annotations.Test;
 import static com.jayway.restassured.RestAssured.get;
 import static com.jayway.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
+import static org.testng.Assert.assertTrue;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.io.StringWriter;
+import java.lang.reflect.Method;
 
 
 public class PartTimer  extends TestBaseSetup{
 	
   private String bearerCode = null;	
-
+  public static final String LINE_SEPARATOR = System.getProperty("line.separator");
+  
+  TestLogger testLog= new TestLogger();
+  
   @BeforeClass
   public void setUp() {
-
+	RestAssured.config = config().logConfig(LogConfig.logConfig().enablePrettyPrinting(true)).sslConfig(SSLConfig.sslConfig().relaxedHTTPSValidation());
 	RestAssured.useRelaxedHTTPSValidation();
     RestAssured.baseURI = "https://staging-api.arrowmepro.com";
-		bearerCode = given().contentType("application/json")
-				.body("{\"grant_type\":\"password\", \"client_id\":\"1\",\"client_secret\":\"WUxNdGi5ptoNVtQ8u3HtT4JoU4wTeK1qX6ZN8uCq\",\"username\":\"chuckcwh+131@gmail.com\",\"password\":\"1234qwer\"}")
-				.log().body().when().post("/oauth/token").then().extract().jsonPath().get("access_token");
-		System.out.println(bearerCode);  
+   
+
     
   }
 
-  @Test
-  public void testStatusCode() {
-   boolean isWaiverAccepted =  given().contentType("application/json").accept("application/json").header("Authorization", "Bearer "+this.bearerCode).log().all().
-    when().
-        get("/ptt/v1/account/full").
-    then().contentType(ContentType.JSON).log().body().
-        assertThat().
-        statusCode(HttpStatus.SC_OK).extract().jsonPath().get("data.isWaiverAccepted");
-   System.out.println(isWaiverAccepted); 
-  }
+  
+	@Test
+	public void getAccountDetails() throws FileNotFoundException {
+		
+		TestLogger.logStep("Post oauth/token");
+		bearerCode = given().config(TestLogger.logConfig()).contentType("application/json")
+				.body("{\"grant_type\":\"password\", \"client_id\":\"1\",\"client_secret\":\"WUxNdGi5ptoNVtQ8u3HtT4JoU4wTeK1qX6ZN8uCq\",\"username\":\"chuckcwh+131@gmail.com\",\"password\":\"1234qwer\"}")
+				.log().all().when().post("/oauth/token").then().extract().jsonPath().get("access_token");
+		
+		TestLogger.logStep("Bearer Token = " + bearerCode);
+
+		TestLogger.logStep("Get account/full");
+		boolean isWaiverAccepted = given().config(TestLogger.logConfig()).contentType("application/json")
+				.accept("application/json").header("Authorization", "Bearer " + this.bearerCode).log().all().when()
+				.get("/ptt/v1/account/full").then().contentType(ContentType.JSON).log().body().assertThat()
+				.statusCode(HttpStatus.SC_OK).extract().jsonPath().get("data.isWaiverAccepted");
+		Assert.assertFalse(isWaiverAccepted);
+	}
+	
+	@Test
+	public void getAccountDetails2() throws FileNotFoundException {
+		
+		TestLogger.logStep("Post oauth/token");
+		bearerCode = given().config(TestLogger.logConfig()).contentType("application/json")
+				.body("{\"grant_type\":\"password\", \"client_id\":\"1\",\"client_secret\":\"WUxNdGi5ptoNVtQ8u3HtT4JoU4wTeK1qX6ZN8uCq\",\"username\":\"chuckcwh+131@gmail.com\",\"password\":\"1234qwer\"}")
+				.log().all().when().post("/oauth/token").then().extract().jsonPath().get("access_token");
+		
+		TestLogger.logStep("Bearer Token = " + bearerCode);
+
+		TestLogger.logStep("Get account/full");
+		boolean isWaiverAccepted = given().config(TestLogger.logConfig()).contentType("application/json")
+				.accept("application/json").header("Authorization", "Bearer " + this.bearerCode).log().all().when()
+				.get("/ptt/v1/account/full").then().contentType(ContentType.JSON).log().body().assertThat()
+				.statusCode(HttpStatus.SC_OK).extract().jsonPath().get("data.isWaiverAccepted");
+		Assert.assertFalse(isWaiverAccepted);
+	}
+  
+	
+  
+	
+  
+
+ 
+  
+  
 
 /*
   @Test
